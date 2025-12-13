b1ebb90ab27b48baddd8c56e4e6785d9e9699402
VVVictt <VVVictt@outlook.com>
2025-12-13 21:35:00 +0800

    贪心算法的commit试验版

--- submit.cpp
+++ submit.cpp
@@ -3,8 +3,6 @@
 
 // Standard Libraries
 #include <cerrno>
-#include <cstdlib>
-#include <ctime>
 #include <iostream>
 #include <vector>
 
@@ -95,7 +93,8 @@ void outputMove(const Move &move);
 namespace Amazons {
 namespace AI {
 Move think(const Board &board, int myColor);
-}
+int evaluate(const Board &board, int myColor);
+} // namespace AI
 } // namespace Amazons
 #endif
 
@@ -226,12 +225,26 @@ void outputMove(const Move &move) {
 namespace Amazons {
 namespace AI {
 Move think(const Board &board, int myColor) {
-  srand(time(0));
   std::vector<Move> moves = Logic::getLegalMoves(board, myColor);
   if (moves.empty())
     return {{-1, -1}, {-1, -1}, {-1, -1}};
-  int index = rand() % moves.size();
-  return moves[index];
+  int bestScore = -1000000;
+  Move bestMove = moves[0];
+  for (auto it = moves.begin(); it != moves.end(); ++it) {
+    Board tempBoard = board;
+    Logic::applyMove(tempBoard, *it, myColor);
+    int value = evaluate(tempBoard, myColor);
+    if (value > bestScore) {
+      bestScore = value;
+      bestMove = *it;
+    }
+  }
+  return bestMove;
+}
+int evaluate(const Board &board, int myColor) {
+  int myMoves = Logic::getLegalMoves(board, myColor).size();
+  int opMoves = Logic::getLegalMoves(board, myColor).size();
+  return (myMoves - opMoves);
 }
 } // namespace AI
 } // namespace Amazons
